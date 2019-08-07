e0847d61fd92cef7a6877c165f6578bb3d62185e
Andrey Somov <public.somov@gmail.com>
2019-08-07 22:31:49 +0300

    Add Javadoc

--- src/main/java/org/snakeyaml/engine/v2/api/SettingKey.java
+++ src/main/java/org/snakeyaml/engine/v2/api/SettingKey.java
@@ -18,7 +18,9 @@ package org.snakeyaml.engine.v2.api;
 import java.io.Serializable;
 
 /**
- * Type for the keys in settings
+ * Type for the custom keys in settings. The keys (used in custom settings) must implement this interface.
+ * It makes possible to be 'compiler safe' and define Enum for keys.
+ * This way is easier to follow the changes in the key names and avoid a typo.
  */
 public interface SettingKey extends Serializable {
 }

8fc792c0ef4f7e088da16af79f4edc1e520e6e10
Andrey Somov <public.somov@gmail.com>
2023-01-18 12:48:44 +0400

    Use the latest Temurin versions

--- docker-run-jdk17.sh
+++ docker-run-jdk17.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./maven-docker-run.sh eclipse-temurin:17.0.5_8-jdk -Preformat $@
+./maven-docker-run.sh eclipse-temurin:17 -Preformat $@
--- docker-run-jdk8.sh
+++ docker-run-jdk8.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./maven-docker-run.sh eclipse-temurin:8u352-b08-jdk $@
+./maven-docker-run.sh eclipse-temurin:8 $@

55d80624042a8ec700cd1e5b3bd97f560a3a8a87
Adam <897017+aSemy@users.noreply.github.com>
2023-05-20 11:52:41 +0200

    convert Token to Kotlin

--- src/main/java/org/snakeyaml/engine/v2/tokens/Token.kt
+++ src/main/java/org/snakeyaml/engine/v2/tokens/Token.kt
@@ -11,78 +11,53 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.v2.tokens;
+package org.snakeyaml.engine.v2.tokens
 
-import java.util.Objects;
-import java.util.Optional;
-import org.snakeyaml.engine.v2.exceptions.Mark;
+import org.snakeyaml.engine.v2.exceptions.Mark
+import java.util.*
 
 /**
  * A unit of YAML data
  */
-public abstract class Token {
-
-  private final Optional<Mark> startMark;
-  private final Optional<Mark> endMark;
-
-  public Token(Optional<Mark> startMark, Optional<Mark> endMark) {
-    Objects.requireNonNull(startMark);
-    Objects.requireNonNull(endMark);
-    this.startMark = startMark;
-    this.endMark = endMark;
-  }
-
-  public Optional<Mark> getStartMark() {
-    return startMark;
-  }
-
-  public Optional<Mark> getEndMark() {
-    return endMark;
-  }
+abstract class Token(
+  val  startMark: Optional<Mark>,
+  val endMark: Optional<Mark>
+) {
 
   /**
    * For error reporting.
    *
    * @return ID of this token
    */
-  public abstract Token.ID getTokenId();
+  abstract val tokenId: ID
 
-  @Override
-  public String toString() {
-    return getTokenId().toString();
-  }
+  override fun toString(): String = tokenId.toString()
 
-  public enum ID {
-    Alias("<alias>"), // NOSONAR
-    Anchor("<anchor>"), // NOSONAR
-    BlockEnd("<block end>"), // NOSONAR
-    BlockEntry("-"), // NOSONAR
-    BlockMappingStart("<block mapping start>"), // NOSONAR
-    BlockSequenceStart("<block sequence start>"), // NOSONAR
-    Directive("<directive>"), // NOSONAR
-    DocumentEnd("<document end>"), // NOSONAR
-    DocumentStart("<document start>"), // NOSONAR
-    FlowEntry(","), // NOSONAR
-    FlowMappingEnd("}"), // NOSONAR
-    FlowMappingStart("{"), // NOSONAR
-    FlowSequenceEnd("]"), // NOSONAR
-    FlowSequenceStart("["), // NOSONAR
-    Key("?"), // NOSONAR
-    Scalar("<scalar>"), // NOSONAR
-    StreamEnd("<stream end>"), // NOSONAR
-    StreamStart("<stream start>"), // NOSONAR
-    Tag("<tag>"), // NOSONAR
-    Comment("#"), Value(":"); // NOSONAR
+  enum class ID(
+    private val description: String
+  ) {
+    Alias("<alias>"),
+    Anchor("<anchor>"),
+    BlockEnd("<block end>"),
+    BlockEntry("-"),
+    BlockMappingStart("<block mapping start>"),
+    BlockSequenceStart("<block sequence start>"),
+    Directive("<directive>"),
+    DocumentEnd("<document end>"),
+    DocumentStart("<document start>"),
+    FlowEntry(","),
+    FlowMappingEnd("}"),
+    FlowMappingStart("{"),
+    FlowSequenceEnd("]"),
+    FlowSequenceStart("["),
+    Key("?"),
+    Scalar("<scalar>"),
+    StreamEnd("<stream end>"),
+    StreamStart("<stream start>"),
+    Tag("<tag>"),
+    Comment("#"),
+    Value(":");
 
-    private final String description;
-
-    ID(String s) {
-      description = s;
-    }
-
-    @Override
-    public String toString() {
-      return description;
-    }
+    override fun toString(): String = description
   }
 }

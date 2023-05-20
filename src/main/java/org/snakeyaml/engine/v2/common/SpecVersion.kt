d3a741a2da39dec6c3a48d47de879b78f2fc982d
Adam <897017+aSemy@users.noreply.github.com>
2023-05-20 15:55:21 +0200

    update SpecVersion

--- src/main/java/org/snakeyaml/engine/v2/common/SpecVersion.kt
+++ src/main/java/org/snakeyaml/engine/v2/common/SpecVersion.kt
@@ -11,64 +11,22 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.v2.common;
+package org.snakeyaml.engine.v2.common
 
-import java.io.Serializable;
+import java.io.Serializable
 
 /**
  * YAML Version indicator
+ * @param major - major part of version, must be 1
+ * @param minor - minor part of version, may be 0 or 1
  */
-public class SpecVersion implements Serializable {
+class SpecVersion(
+    val major: Int,
+    val minor: Int,
+) : Serializable {
+    /** create readable text */
+    val representation: String
+        get() = "$major.$minor"
 
-  /**
-   * keep major
-   */
-  private final int major;
-  /**
-   * keep minor
-   */
-  private final int minor;
-
-  /**
-   * Create
-   *
-   * @param major - major part ov version, must be 1
-   * @param minor - minor part of version, may be 0 or 1
-   */
-  public SpecVersion(int major, int minor) {
-    this.major = major;
-    this.minor = minor;
-  }
-
-  /**
-   * getter
-   *
-   * @return 1
-   */
-  public int getMajor() {
-    return major;
-  }
-
-  /**
-   * getter
-   *
-   * @return 0 or 1
-   */
-  public int getMinor() {
-    return minor;
-  }
-
-  /**
-   * create readable text
-   *
-   * @return text
-   */
-  public String getRepresentation() {
-    return major + "." + minor;
-  }
-
-  @Override
-  public String toString() {
-    return "Version{" + "major=" + major + ", minor=" + minor + '}';
-  }
+    override fun toString(): String = "Version{major=$major, minor=$minor}"
 }

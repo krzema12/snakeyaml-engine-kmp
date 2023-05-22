bb82d73abd206b0eca3decdadad8dad2b5824c27
Piotr Krzeminski <git@krzeminski.it>
2023-05-22 11:34:44 +0200

    Convert CommentEventsCollector

--- src/main/java/org/snakeyaml/engine/v2/comments/CommentEventsCollector.kt
+++ src/main/java/org/snakeyaml/engine/v2/comments/CommentEventsCollector.kt
@@ -11,165 +11,153 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.v2.comments;
+package org.snakeyaml.engine.v2.comments
 
-import org.jetbrains.annotations.Nullable;
-import org.snakeyaml.engine.v2.events.CommentEvent;
-import org.snakeyaml.engine.v2.events.Event;
-import org.snakeyaml.engine.v2.parser.Parser;
-
-import java.util.*;
+import org.snakeyaml.engine.v2.events.CommentEvent
+import org.snakeyaml.engine.v2.events.Event
+import org.snakeyaml.engine.v2.parser.Parser
+import java.util.*
 
 /**
  * Used by the Composer and Emitter to collect comment events so that they can be used at a later
  * point in the process.
  */
-public class CommentEventsCollector {
+class CommentEventsCollector {
+    private val eventSource: Queue<Event>
+    private val expectedCommentTypes: Array<CommentType>
+    private var commentLineList: MutableList<CommentLine>
 
-  private final Queue<Event> eventSource;
-  private final CommentType[] expectedCommentTypes;
-  private List<CommentLine> commentLineList;
+    /**
+     * Constructor used to collect comment events emitted by a Parser.
+     *
+     * @param parser               the event source.
+     * @param expectedCommentTypes the comment types expected. Any comment types not included are not
+     * collected.
+     */
+    constructor(parser: Parser, vararg expectedCommentTypes: CommentType) {
+        eventSource = object : AbstractQueue<Event>() {
+            override fun offer(e: Event): Boolean {
+                throw UnsupportedOperationException()
+            }
 
-  /**
-   * Constructor used to collect comment events emitted by a Parser.
-   *
-   * @param parser               the event source.
-   * @param expectedCommentTypes the comment types expected. Any comment types not included are not
-   *                             collected.
-   */
-  public CommentEventsCollector(final Parser parser, CommentType... expectedCommentTypes) {
-    this.eventSource = new AbstractQueue<Event>() {
+            override fun poll(): Event {
+                return parser.next()
+            }
 
-      @Override
-      public boolean offer(Event e) {
-        throw new UnsupportedOperationException();
-      }
+            override fun peek(): Event {
+                return parser.peekEvent()
+            }
 
-      @Override
-      public Event poll() {
-        return parser.next();
-      }
+            override fun iterator(): MutableIterator<Event> {
+                throw UnsupportedOperationException()
+            }
 
-      @Override
-      public Event peek() {
-        return parser.peekEvent();
-      }
-
-      @Override
-      public Iterator<Event> iterator() {
-        throw new UnsupportedOperationException();
-      }
-
-      @Override
-      public int size() {
-        throw new UnsupportedOperationException();
-      }
-
-    };
-    this.expectedCommentTypes = expectedCommentTypes;
-    commentLineList = new ArrayList<>();
-  }
-
-  /**
-   * Constructor used to collect events emitted by the Serializer.
-   *
-   * @param eventSource          the event source.
-   * @param expectedCommentTypes the comment types expected. Any comment types not included are not
-   *                             collected.
-   */
-  public CommentEventsCollector(Queue<Event> eventSource, CommentType... expectedCommentTypes) {
-    this.eventSource = eventSource;
-    this.expectedCommentTypes = expectedCommentTypes;
-    commentLineList = new ArrayList<>();
-  }
-
-  /**
-   * Determine if the event is a comment of one of the expected types set during construction.
-   *
-   * @param event the event to test.
-   * @return <code>true</code> if the events is a comment of the expected type; Otherwise, false.
-   */
-  private boolean isEventExpected(@Nullable Event event) {
-    if (event == null || event.getEventId() != Event.ID.Comment) {
-      return false;
+            override val size: Int
+                get() = throw UnsupportedOperationException()
+        }
+        this.expectedCommentTypes = expectedCommentTypes as Array<CommentType>
+        commentLineList = ArrayList()
     }
-    CommentEvent commentEvent = (CommentEvent) event;
-    for (CommentType type : expectedCommentTypes) {
-      if (commentEvent.getCommentType() == type) {
-        return true;
-      }
+
+    /**
+     * Constructor used to collect events emitted by the Serializer.
+     *
+     * @param eventSource          the event source.
+     * @param expectedCommentTypes the comment types expected. Any comment types not included are not
+     * collected.
+     */
+    constructor(eventSource: Queue<Event>, vararg expectedCommentTypes: CommentType) {
+        this.eventSource = eventSource
+        this.expectedCommentTypes = expectedCommentTypes as Array<CommentType>
+        commentLineList = ArrayList()
     }
-    return false;
-  }
 
-  /**
-   * Collect all events of the expected type (set during construction) starting with the top event
-   * on the event source. Collection stops as soon as a non comment or comment of the unexpected
-   * type is encountered.
-   *
-   * @return this object.
-   */
-  public CommentEventsCollector collectEvents() {
-    collectEvents(null);
-    return this;
-  }
-
-  /**
-   * Collect all events of the expected type (set during construction) starting with event provided
-   * as an argument and continuing with the top event on the event source. Collection stops as soon
-   * as a non comment or comment of the unexpected type is encountered.
-   *
-   * @param event the first event to attempt to collect.
-   * @return the event provided as an argument, if it is not collected; Otherwise, <code>null</code>
-   */
-  public Event collectEvents(Event event) {
-    if (event != null) {
-      if (isEventExpected(event)) {
-        commentLineList.add(new CommentLine((CommentEvent) event));
-      } else {
-        return event;
-      }
+    /**
+     * Determine if the event is a comment of one of the expected types set during construction.
+     *
+     * @param event the event to test.
+     * @return `true` if the events is a comment of the expected type; Otherwise, false.
+     */
+    private fun isEventExpected(event: Event?): Boolean {
+        if (event == null || event.eventId !== Event.ID.Comment) {
+            return false
+        }
+        val commentEvent = event as CommentEvent
+        for (type in expectedCommentTypes) {
+            if (commentEvent.commentType == type) {
+                return true
+            }
+        }
+        return false
     }
-    while (isEventExpected(eventSource.peek())) {
-      final Event e = Objects.requireNonNull(eventSource.poll());
-      commentLineList.add(new CommentLine((CommentEvent) e));
+
+    /**
+     * Collect all events of the expected type (set during construction) starting with the top event
+     * on the event source. Collection stops as soon as a non comment or comment of the unexpected
+     * type is encountered.
+     *
+     * @return this object.
+     */
+    fun collectEvents(): CommentEventsCollector {
+        collectEvents(null)
+        return this
     }
-    return null;
-  }
 
-  /**
-   * Collect all events of the expected type (set during construction) starting with event provided
-   * as an argument and continuing with the top event on the event source. Collection stops as soon
-   * as a non comment or comment of the unexpected type is encountered.
-   *
-   * @param event the first event to attempt to collect.
-   * @return the event provided as an argument, if it is not collected; Otherwise, the first event
-   * that is not collected.
-   */
-  public Event collectEventsAndPoll(Event event) {
-    Event nextEvent = collectEvents(event);
-    return nextEvent != null ? nextEvent : eventSource.poll();
-  }
-
-  /**
-   * Return the events collected and reset the collector.
-   *
-   * @return the events collected.
-   */
-  public List<CommentLine> consume() {
-    try {
-      return commentLineList;
-    } finally {
-      commentLineList = new ArrayList<>();
+    /**
+     * Collect all events of the expected type (set during construction) starting with event provided
+     * as an argument and continuing with the top event on the event source. Collection stops as soon
+     * as a non comment or comment of the unexpected type is encountered.
+     *
+     * @param event the first event to attempt to collect.
+     * @return the event provided as an argument, if it is not collected; Otherwise, `null`
+     */
+    fun collectEvents(event: Event?): Event? {
+        if (event != null) {
+            if (isEventExpected(event)) {
+                commentLineList.add(CommentLine(event as CommentEvent?))
+            } else {
+                return event
+            }
+        }
+        while (isEventExpected(eventSource.peek())) {
+            val e = Objects.requireNonNull(eventSource.poll())
+            commentLineList.add(CommentLine(e as CommentEvent))
+        }
+        return null
     }
-  }
 
-  /**
-   * Test if the collector contains any collected events.
-   *
-   * @return <code>true</code> if it does; Otherwise, <code>false</code>
-   */
-  public boolean isEmpty() {
-    return commentLineList.isEmpty();
-  }
+    /**
+     * Collect all events of the expected type (set during construction) starting with event provided
+     * as an argument and continuing with the top event on the event source. Collection stops as soon
+     * as a non comment or comment of the unexpected type is encountered.
+     *
+     * @param event the first event to attempt to collect.
+     * @return the event provided as an argument, if it is not collected; Otherwise, the first event
+     * that is not collected.
+     */
+    fun collectEventsAndPoll(event: Event?): Event {
+        val nextEvent = collectEvents(event)
+        return nextEvent ?: eventSource.poll()
+    }
+
+    /**
+     * Return the events collected and reset the collector.
+     *
+     * @return the events collected.
+     */
+    fun consume(): List<CommentLine> {
+        return try {
+            commentLineList
+        } finally {
+            commentLineList = ArrayList()
+        }
+    }
+
+    val isEmpty: Boolean
+        /**
+         * Test if the collector contains any collected events.
+         *
+         * @return `true` if it does; Otherwise, `false`
+         */
+        get() = commentLineList.isEmpty()
 }

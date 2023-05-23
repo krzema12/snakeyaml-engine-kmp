284f0f5722c60a2f681ea644a6300cf1aaadc126
Adam <897017+aSemy@users.noreply.github.com>
2023-05-23 12:08:28 +0200

    convert Scanner to Kotlin

--- src/main/java/org/snakeyaml/engine/v2/scanner/Scanner.kt
+++ src/main/java/org/snakeyaml/engine/v2/scanner/Scanner.kt
@@ -11,57 +11,52 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.v2.scanner;
+package org.snakeyaml.engine.v2.scanner
 
-
-import java.util.Iterator;
-import org.snakeyaml.engine.v2.exceptions.ScannerException;
-import org.snakeyaml.engine.v2.tokens.Token;
+import org.snakeyaml.engine.v2.exceptions.ScannerException
+import org.snakeyaml.engine.v2.tokens.Token
 
 /**
- * This interface represents an input stream of {@link Token}s.
- * <p>
- * The scanner and the parser form together the 'Parse' step in the loading process.
- * </p>
+ * This interface represents an input stream of [Token]s.
  *
- * @see <a href="https://yaml.org/spec/1.2.2/#31-processes">3.1. Processes</a>
+ * The scanner and the parser form together the 'Parse' step in the loading process.
+ *
+ * @see [3.1. Processes](https://yaml.org/spec/1.2.2/.31-processes)
  */
-public interface Scanner extends Iterator<Token> {
+interface Scanner : Iterator<Token> {
+    /**
+     * Check if the next token is one of the given types.
+     *
+     * @param choices token IDs to match with
+     * @return `true` if the next token is one of the given types. Returns
+     * `false` if no more tokens are available.
+     * @throws ScannerException Thrown in case of malformed input.
+     */
+    fun checkToken(vararg choices: Token.ID): Boolean
 
-  /**
-   * Check if the next token is one of the given types.
-   *
-   * @param choices token IDs to match with
-   * @return <code>true</code> if the next token is one of the given types. Returns
-   *         <code>false</code> if no more tokens are available.
-   * @throws ScannerException Thrown in case of malformed input.
-   */
-  boolean checkToken(Token.ID... choices);
+    /**
+     * Return the next token, but do not delete it from the stream.
+     *
+     * @return The token that will be returned on the next call to [next]
+     * @throws ScannerException Thrown in case of malformed input.
+     * @throws IndexOutOfBoundsException if no more token left
+     */
+    fun peekToken(): Token?
 
-  /**
-   * Return the next token, but do not delete it from the stream.
-   *
-   * @return The token that will be returned on the next call to {@link #next}
-   * @throws ScannerException Thrown in case of malformed input.
-   * @throws IndexOutOfBoundsException if no more token left
-   */
-  Token peekToken();
+    /**
+     * Returns the next token.
+     *
+     * The token will be removed from the stream. (Every invocation of this method must happen after
+     * calling either [checkToken] or [peekToken]
+     *
+     * @return the coming token
+     * @throws ScannerException Thrown in case of malformed input.
+     * @throws IndexOutOfBoundsException if no more token left
+     */
+    override fun next(): Token
 
-  /**
-   * Returns the next token.
-   * <p>
-   * The token will be removed from the stream. (Every invocation of this method must happen after
-   * calling either {@link #checkToken} or {@link #peekToken()}
-   * </p>
-   *
-   * @return the coming token
-   * @throws ScannerException Thrown in case of malformed input.
-   * @throws IndexOutOfBoundsException if no more token left
-   */
-  Token next();
-
-  /**
-   * Set the document index to 0 after a document end
-   */
-  void resetDocumentIndex();
+    /**
+     * Set the document index to `0` after a document end
+     */
+    fun resetDocumentIndex()
 }

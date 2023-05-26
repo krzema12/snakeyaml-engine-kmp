412cf86b0cf895b2164d88fc1ac092a841d00c5e
Adam <897017+aSemy@users.noreply.github.com>
2023-05-26 18:00:25 +0200

    minor tidyings

--- src/main/java/org/snakeyaml/engine/v2/exceptions/YamlVersionException.kt
+++ src/main/java/org/snakeyaml/engine/v2/exceptions/YamlVersionException.kt
@@ -20,5 +20,5 @@ import org.snakeyaml.engine.v2.common.SpecVersion
  * @param specVersion - the specified version
  */
 class YamlVersionException(
-    val specVersion: SpecVersion,
+    specVersion: SpecVersion,
 ) : YamlEngineException(specVersion.toString())
--- src/main/java/org/snakeyaml/engine/v2/resolver/CoreScalarResolver.kt
+++ src/main/java/org/snakeyaml/engine/v2/resolver/CoreScalarResolver.kt
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.v2.resolver;
+package org.snakeyaml.engine.v2.resolver
 
 import org.snakeyaml.engine.v2.nodes.Tag
 import java.util.regex.Pattern
@@ -21,20 +21,20 @@ import java.util.regex.Pattern
  */
 class CoreScalarResolver : BaseScalarResolver(
     {
-        addImplicitResolver(Tag.NULL, EMPTY, null);
-        addImplicitResolver(Tag.BOOL, BOOL, "tfTF");
+        addImplicitResolver(Tag.NULL, EMPTY, null)
+        addImplicitResolver(Tag.BOOL, BOOL, "tfTF")
         // INT must be before FLOAT because the regular expression for FLOAT matches INT
-        addImplicitResolver(Tag.INT, INT, "-+0123456789");
-        addImplicitResolver(Tag.FLOAT, FLOAT, "-+0123456789.");
-        addImplicitResolver(Tag.NULL, NULL, "n\u0000");
-        addImplicitResolver(Tag.ENV_TAG, ENV_FORMAT.toPattern(), "$");
+        addImplicitResolver(Tag.INT, INT, "-+0123456789")
+        addImplicitResolver(Tag.FLOAT, FLOAT, "-+0123456789.")
+        addImplicitResolver(Tag.NULL, NULL, "n\u0000")
+        addImplicitResolver(Tag.ENV_TAG, ENV_FORMAT.toPattern(), "$")
     },
 ) {
 
     companion object {
 
         /** Boolean as defined in Core */
-        val BOOL: Pattern = Pattern.compile("^(?:true|True|TRUE|false|False|FALSE)$");
+        val BOOL: Pattern = Pattern.compile("^(?:true|True|TRUE|false|False|FALSE)$")
 
         /**
          * Float as defined in JSON (Number which is Float).
@@ -54,10 +54,10 @@ class CoreScalarResolver : BaseScalarResolver(
             "^([-+]?[0-9]+)" + // (base 10)
                 "|(0o[0-7]+)" + // (base 8)
                 "|(0x[0-9a-fA-F]+)$", // (base 16)
-        );
+        )
 
         /** Null as defined in Core */
-        val NULL: Pattern = Pattern.compile("^(?:~|null|Null|NULL| )$");
+        val NULL: Pattern = Pattern.compile("^(?:~|null|Null|NULL| )$")
 
     }
 }

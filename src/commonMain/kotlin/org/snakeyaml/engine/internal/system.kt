c80b475abca1ecfded38a5dd8e89e000f366edd8
Adam <897017+aSemy@users.noreply.github.com>
2023-06-05 12:23:02 +0200

    add `identityHashCode()` util

--- src/commonMain/kotlin/org/snakeyaml/engine/internal/system.kt
+++ src/commonMain/kotlin/org/snakeyaml/engine/internal/system.kt
@@ -1,6 +1,13 @@
 package org.snakeyaml.engine.internal
 
+import kotlin.jvm.JvmInline
+
 /**
  * Fetch an environment variable from the current system.
  */
 internal expect fun getEnvironmentVariable(key: String): String?
+
+@JvmInline
+internal value class IdentityHashCode(private val value: Int)
+
+internal expect fun identityHashCode(any: Any?): IdentityHashCode
--- src/jvmMain/kotlin/org/snakeyaml/engine/internal/system.jvm.kt
+++ src/jvmMain/kotlin/org/snakeyaml/engine/internal/system.jvm.kt
@@ -4,3 +4,8 @@ package org.snakeyaml.engine.internal
  * Fetch an environment variable using [System.getenv]
  */
 internal actual fun getEnvironmentVariable(key: String): String? = System.getenv(key)
+
+internal actual fun identityHashCode(any: Any?): IdentityHashCode {
+    val hc = System.identityHashCode(any)
+    return IdentityHashCode(hc)
+}

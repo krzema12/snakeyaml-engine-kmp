1876250c8f9a2423424870e47aa7ccdadf19e5de
Adam <897017+aSemy@users.noreply.github.com>
2023-06-15 10:42:38 +0100

    Set up Maven Publish settings (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     buildsrc.conventions.lang.`kotlin-multiplatform-jvm`
     buildsrc.conventions.lang.`kotlin-multiplatform-js`
     buildsrc.conventions.lang.`kotlin-multiplatform-native`
+    buildsrc.conventions.publishing
     buildsrc.conventions.`git-branch-publish`
 }
 

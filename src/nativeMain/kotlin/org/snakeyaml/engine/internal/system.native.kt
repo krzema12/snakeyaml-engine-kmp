160ba3a80fd57b480bdd211ab2ce9dd66a12bcc4
Piotr Krzeminski <git@krzeminski.it>
2023-09-18 07:47:16 +0200

    Opt into ExperimentalForeignApi

--- src/nativeMain/kotlin/org/snakeyaml/engine/internal/system.native.kt
+++ src/nativeMain/kotlin/org/snakeyaml/engine/internal/system.native.kt
@@ -1,9 +1,11 @@
 package org.snakeyaml.engine.internal
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.toKStringFromUtf8
 import platform.posix.getenv
 import kotlin.native.identityHashCode
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual fun getEnvironmentVariable(key: String): String? =
     getenv(key)?.toKStringFromUtf8()
 

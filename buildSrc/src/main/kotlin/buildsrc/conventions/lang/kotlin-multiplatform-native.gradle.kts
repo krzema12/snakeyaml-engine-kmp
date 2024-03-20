0a0fdf2350e205604adac15e1ddef5409bb9699a
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-03-20 08:24:47 +0100

    Enable more native targets

--- buildSrc/src/main/kotlin/buildsrc/conventions/lang/kotlin-multiplatform-native.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/conventions/lang/kotlin-multiplatform-native.gradle.kts
@@ -14,14 +14,11 @@ kotlin {
     macosX64()
     macosArm64()
 
-    // More specialised targets are disabled for now, since I anticipate low demand.
-    // They can be re-enabled, if there is demand for them.
-    // https://kotlinlang.org/docs/multiplatform-share-on-platforms.html#use-target-shortcuts
-    //ios()     // iosArm64, iosX64
-    //watchos() // watchosArm32, watchosArm64, watchosX64
-    //tvos()    // tvosArm64, tvosX64
+    ios()     // iosArm64, iosX64
+    watchos() // watchosArm32, watchosArm64, watchosX64
+    tvos()    // tvosArm64, tvosX64
 
-    //iosSimulatorArm64()
-    //tvosSimulatorArm64()
-    //watchosSimulatorArm64()
+    iosSimulatorArm64()
+    tvosSimulatorArm64()
+    watchosSimulatorArm64()
 }

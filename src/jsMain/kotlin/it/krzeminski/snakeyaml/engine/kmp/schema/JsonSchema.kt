742fbdf3a86f5fe753bb2b985f2bb5d4bf7bf712
Adam <897017+aSemy@users.noreply.github.com>
2024-05-10 15:20:17 +0200

    refactor JsonSchema to avoid unnecessary expect/actual class
    JsonSchema doesn't need to be an expect/actual class.
    
    Instead, refactor the platform-specific Tag/Constructor map to an expect/actual function.
    
    This avoids the "expect/actual classes are incubating" warning for JsonSchema.

--- src/jsMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/schema/JsonSchema.kt
+++ src/jsMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/schema/JsonSchema.kt
@@ -1,27 +1,10 @@
 package it.krzeminski.snakeyaml.engine.kmp.schema
 
 import it.krzeminski.snakeyaml.engine.kmp.api.ConstructNode
-import it.krzeminski.snakeyaml.engine.kmp.constructor.ConstructYamlNull
-import it.krzeminski.snakeyaml.engine.kmp.constructor.json.ConstructYamlBinary
-import it.krzeminski.snakeyaml.engine.kmp.constructor.json.ConstructYamlJsonBool
-import it.krzeminski.snakeyaml.engine.kmp.constructor.json.ConstructYamlJsonFloat
-import it.krzeminski.snakeyaml.engine.kmp.constructor.json.ConstructYamlJsonInt
 import it.krzeminski.snakeyaml.engine.kmp.nodes.Tag
 import it.krzeminski.snakeyaml.engine.kmp.resolver.ScalarResolver
 
-/**
- * Default schema for Kotlin/JS
- */
-actual open class JsonSchema actual constructor(
-    override val scalarResolver: ScalarResolver,
-) : Schema {
-
-    /** Basic constructs */
-    override val schemaTagConstructors: Map<Tag, ConstructNode> = mapOf(
-        Tag.NULL to ConstructYamlNull(),
-        Tag.BOOL to ConstructYamlJsonBool(),
-        Tag.INT to ConstructYamlJsonInt(),
-        Tag.FLOAT to ConstructYamlJsonFloat(),
-        Tag.BINARY to ConstructYamlBinary(),
-    )
-}
+/** Basic constructs for Kotlin/JS */
+internal actual fun targetSchemaTagConstructors(
+    scalarResolver: ScalarResolver
+): Map<Tag, ConstructNode> = emptyMap()
--- src/jvmMain/java/it/krzeminski/snakeyaml/engine/kmp/schema/JsonSchema.kt
+++ src/jvmMain/java/it/krzeminski/snakeyaml/engine/kmp/schema/JsonSchema.kt
@@ -14,26 +14,16 @@
 package it.krzeminski.snakeyaml.engine.kmp.schema
 
 import it.krzeminski.snakeyaml.engine.kmp.api.ConstructNode
-import it.krzeminski.snakeyaml.engine.kmp.constructor.ConstructYamlNull
-import it.krzeminski.snakeyaml.engine.kmp.constructor.json.*
+import it.krzeminski.snakeyaml.engine.kmp.constructor.json.ConstructOptionalClass
+import it.krzeminski.snakeyaml.engine.kmp.constructor.json.ConstructUuidClass
 import it.krzeminski.snakeyaml.engine.kmp.nodes.Tag
 import it.krzeminski.snakeyaml.engine.kmp.resolver.ScalarResolver
 
-/**
- * Default schema for Kotlin/JVM
- */
-actual open class JsonSchema actual constructor(
-    final override val scalarResolver: ScalarResolver,
-) : Schema {
-
-    /** Basic constructs */
-    override val schemaTagConstructors: Map<Tag, ConstructNode> = mapOf(
-        Tag.NULL to ConstructYamlNull(),
-        Tag.BOOL to ConstructYamlJsonBool(),
-        Tag.INT to ConstructYamlJsonInt(),
-        Tag.FLOAT to ConstructYamlJsonFloat(),
-        Tag.BINARY to ConstructYamlBinary(),
+/** Basic constructs for Kotlin/JVM */
+internal actual fun targetSchemaTagConstructors(
+    scalarResolver: ScalarResolver
+): Map<Tag, ConstructNode> =
+    mapOf(
         Tag.forType("java.util.UUID") to ConstructUuidClass(),
         Tag.forType("java.util.Optional") to ConstructOptionalClass(scalarResolver),
     )
-}

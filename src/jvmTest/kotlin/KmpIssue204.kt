d8600581daa4b828d5d74e1a6de6f449825f85cd
Adam <897017+aSemy@users.noreply.github.com>
2024-06-28 15:15:58 +0200

    relax performance assertion in KmpIssue204

--- src/jvmTest/kotlin/KmpIssue204.kt
+++ src/jvmTest/kotlin/KmpIssue204.kt
@@ -6,11 +6,13 @@ import io.kotest.matchers.maps.shouldHaveSize
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.resource.resourceAsString
 import it.krzeminski.snakeyaml.engine.kmp.api.Load
-import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.measureTime
 
 class KmpIssue204 : FunSpec({
     test("large yaml should be parsed quickly") {
+        // quick and dirty performance test - should be replaced by a proper performance test eventually
+
         val openAiApiYaml = resourceAsString("/issues/kmp-issue-204-OpenAI-API.yaml")
         val duration = measureTime {
             val loader = Load()
@@ -18,6 +20,6 @@ class KmpIssue204 : FunSpec({
             value.shouldNotBeNull()
             value shouldHaveSize 8
         }
-        duration shouldBeLessThan 500.milliseconds
+        duration shouldBeLessThan 1.seconds
     }
 })

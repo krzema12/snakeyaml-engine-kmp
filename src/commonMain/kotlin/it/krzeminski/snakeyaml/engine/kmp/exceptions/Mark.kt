b504b17f52c9abc1f93b5fc794ef9908f3a11ea1
Oleg <oleg.smirnov@exactprosystems.com>
2024-06-28 20:54:20 +0400

    Remove lazy. Recompute array on each call as this is a deprecated property

--- src/commonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/exceptions/Mark.kt
+++ src/commonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/exceptions/Mark.kt
@@ -39,7 +39,8 @@ class Mark @JvmOverloads constructor(
 ) {
 
     @Deprecated("Converted to immutable List<Int>, replace with `codepoints`")
-    val buffer: IntArray by lazy { codepoints.toIntArray() }
+    val buffer: IntArray
+        get() = codepoints.toIntArray()
 
     /**
      * Deprecated: please convert [str] to codepoints.

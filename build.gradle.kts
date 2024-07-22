0799bc9ff34402078be2f5e09cdccdfb761b4111
Piotr Krzeminski <git@krzeminski.it>
2024-07-22 09:30:42 +0200

    Add JVM API dump

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     buildsrc.conventions.`git-branch-publish`
     buildsrc.conventions.`yaml-testing`
     id("dev.adamko.dokkatoo-html") version "2.3.1"
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.2"
 }
 
 group = "it.krzeminski"

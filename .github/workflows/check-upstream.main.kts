c2590efd06893c83e701c797de32c5bbd7c59267
Piotr Krzeminski <git@krzeminski.it>
2024-11-07 14:22:37 +0100

    Reuse Maven Central badge's style

--- .github/workflows/check-upstream.main.kts
+++ .github/workflows/check-upstream.main.kts
@@ -4,11 +4,16 @@
 
 @file:Repository("https://bindings.krzeminski.it/")
 @file:DependsOn("actions:checkout:v4")
+@file:OptIn(ExperimentalKotlinLogicStep::class)
 
 import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.annotations.ExperimentalKotlinLogicStep
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
 import io.github.typesafegithub.workflows.dsl.workflow
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
 
 workflow(
     name = "Check upstream",
@@ -32,13 +37,36 @@ workflow(
         )
         run(
             name = "Create an SVG with the number of commits",
-            command = """
-                echo "<svg viewBox=\"0 0 85 20\" xmlns=\"http://www.w3.org/2000/svg\"><style>.number { font: 13px sans-serif; }</style><text x=\"0\" y=\"0\" class=\"number\">${'$'}(cat number-of-commits.txt)</text></svg>" > badge.svg
-            """.trimIndent(),
-        )
+        ) {
+            val numberOfCommits = Path("number-of-commits.txt").readText().trim()
+            val badgeAsSvg = """
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="20" role="img" aria-label="To upstream: $numberOfCommits">
+                  <title>To upstream: $numberOfCommits</title>
+                  <linearGradient id="s" x2="0" y2="100%">
+                    <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+                    <stop offset="1" stop-opacity=".1"/>
+                  </linearGradient>
+                  <clipPath id="r">
+                    <rect width="128" height="20" rx="3" fill="#fff"/>
+                  </clipPath>
+                  <g clip-path="url(#r)">
+                    <rect width="89" height="20" fill="#555"/>
+                    <rect x="89" width="39" height="20" fill="#4c1"/>
+                    <rect width="128" height="20" fill="url(#s)"/>
+                  </g>
+                  <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
+                    <text aria-hidden="true" x="455" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="790">To upstream</text>
+                    <text x="455" y="140" transform="scale(.1)" fill="#fff" textLength="790">To upstream</text>
+                    <text aria-hidden="true" x="1075" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)">$numberOfCommits</text>
+                    <text x="1075" y="140" transform="scale(.1)" fill="#fff">$numberOfCommits</text>
+                  </g>
+                </svg>
+            """.trimIndent()
+            Path("commits-to-upstream-badge.svg").writeText(badgeAsSvg)
+        }
         run(
             name = "Preview badge",
-            command = "cat badge.svg",
+            command = "cat commits-to-upstream-badge.svg",
         )
     }
 }

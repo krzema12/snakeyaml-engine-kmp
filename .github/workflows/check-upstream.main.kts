43ba9da0decbad24291a07399ff0da20e3fbb70a
Piotr Krzeminski <git@krzeminski.it>
2024-11-08 07:32:50 +0100

    Use badge from shields.io

--- .github/workflows/check-upstream.main.kts
+++ .github/workflows/check-upstream.main.kts
@@ -4,21 +4,16 @@
 
 @file:Repository("https://bindings.krzeminski.it/")
 @file:DependsOn("actions:checkout:v4")
-@file:OptIn(ExperimentalKotlinLogicStep::class)
 
 import io.github.typesafegithub.workflows.actions.actions.Checkout
-import io.github.typesafegithub.workflows.actions.actions.Checkout.FetchDepth.Infinite
-import io.github.typesafegithub.workflows.annotations.ExperimentalKotlinLogicStep
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.domain.triggers.Cron
 import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.domain.triggers.Schedule
 import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
 import io.github.typesafegithub.workflows.dsl.workflow
-import kotlin.io.path.Path
-import kotlin.io.path.readText
-import kotlin.io.path.writeText
 
+val numberOfCommitsFileName = "number-of-commits.txt"
 val badgeFileName = "commits-to-upstream-badge.svg"
 
 workflow(
@@ -34,44 +29,19 @@ workflow(
         id = "check",
         runsOn = RunnerType.UbuntuLatest,
     ) {
-        uses(action = Checkout(fetchDepth = Infinite))
+        uses(action = Checkout(ref = "commits-to-upstream-badge"))
         run(
             name = "Clone snakeyaml-engine and check for changes",
             command = """
                 git clone --branch master --single-branch https://bitbucket.org/snakeyaml/snakeyaml-engine.git
                 cd snakeyaml-engine
-                git log --oneline $(cat ../upstream-commit.txt)..master | wc -l > ../number-of-commits.txt
+                git log --oneline $(cat ../upstream-commit.txt)..master | wc -l > ../$numberOfCommitsFileName
             """.trimIndent(),
         )
         run(
             name = "Create an SVG with the number of commits",
-        ) {
-            val numberOfCommits = Path("number-of-commits.txt").readText().trim()
-            val badgeAsSvg = """
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="20" role="img" aria-label="To upstream: $numberOfCommits">
-                  <title>To upstream: $numberOfCommits</title>
-                  <linearGradient id="s" x2="0" y2="100%">
-                    <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
-                    <stop offset="1" stop-opacity=".1"/>
-                  </linearGradient>
-                  <clipPath id="r">
-                    <rect width="128" height="20" rx="3" fill="#fff"/>
-                  </clipPath>
-                  <g clip-path="url(#r)">
-                    <rect width="89" height="20" fill="#555"/>
-                    <rect x="89" width="39" height="20" fill="#4cc"/>
-                    <rect width="128" height="20" fill="url(#s)"/>
-                  </g>
-                  <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
-                    <text aria-hidden="true" x="455" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="790">To upstream</text>
-                    <text x="455" y="140" transform="scale(.1)" fill="#fff" textLength="790">To upstream</text>
-                    <text aria-hidden="true" x="1075" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)">$numberOfCommits</text>
-                    <text x="1075" y="140" transform="scale(.1)" fill="#fff">$numberOfCommits</text>
-                  </g>
-                </svg>
-            """.trimIndent()
-            Path(badgeFileName).writeText(badgeAsSvg)
-        }
+            command = "wget https://img.shields.io/badge/To%20upstream-$(cat $numberOfCommitsFileName)-blue > $badgeFileName",
+        )
         run(
             name = "Preview badge",
             command = "cat $badgeFileName",
@@ -82,18 +52,6 @@ workflow(
                 git config --global user.email "<>"
                 git config --global user.name "GitHub Actions Bot"
 
-                # Pretty complicated... Dealing with conflicts when changing branches,
-                # it probably could be simplified.
-
-                git add $badgeFileName
-                git stash
-                git checkout commits-to-upstream-badge
-
-                rm $badgeFileName
-                git add $badgeFileName
-                git commit -m "Remove previous badge"
-
-                git stash pop
                 git add $badgeFileName
                 git commit --allow-empty -m "Regenerate badge"
                 git push

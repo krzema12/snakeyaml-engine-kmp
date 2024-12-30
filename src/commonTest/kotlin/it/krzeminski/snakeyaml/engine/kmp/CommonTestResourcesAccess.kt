2b956d712c1379ae5cbbbfa6127fc8c3f504f21f
Piotr Krzeminski <git@krzeminski.it>
2024-12-30 09:37:16 +0100

    Extra logging for Windows

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/CommonTestResourcesAccess.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/CommonTestResourcesAccess.kt
@@ -4,11 +4,15 @@ import CommonTestResources
 import okio.ByteString
 
 fun stringFromResources(path: String): String {
+    println("Loading resource: $path")
     val segments = path.split("/")
+    println("Segments: $segments")
     return (segments
         .dropLast(1)
         .fold(CommonTestResources().resourcesMap) { map, segment ->
             @Suppress("UNCHECKED_CAST")
-            map[segment] as Map<String, Any>
+            val next = map[segment] as Map<String, Any>
+            println("Going deeper: $segment -> $next")
+            next
         }[segments.last()] as ByteString).utf8()
 }

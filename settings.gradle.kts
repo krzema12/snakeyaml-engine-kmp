c45dac333b6628fc0f3f440bc5edb3c12bc43236
Piotr Krzeminski <git@krzeminski.it>
2025-01-03 08:57:26 +0100

    Publish build scans in case of failures

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -55,6 +55,17 @@ dependencyResolutionManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
+    id("com.gradle.develocity") version "3.19"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf {
+            System.getenv("GITHUB_ACTIONS") == "true" && it.buildResult.failures.isNotEmpty()
+        }
+    }
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")

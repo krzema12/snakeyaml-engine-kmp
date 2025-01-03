f11e2b1391ea083242fb10fdd8e0f56c5504e901
Piotr Krzeminski <git@krzeminski.it>
2025-01-03 11:15:41 +0100

    Register resources dir as input to fix caching

--- buildSrc/src/main/kotlin/buildsrc/conventions/multiplatform-test-resources.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/conventions/multiplatform-test-resources.gradle.kts
@@ -13,5 +13,8 @@ val convertCommonTestResourcesToKotlin by tasks.registering(buildsrc.tasks.Conve
     description = "Generate Kotlin code for accessing the common test resources"
     group = "verification"
 
+    // HACK: Ideally we'd retrieve the 'resources' path via Gradle API, but somehow Gradle sees only 'main' and 'test'
+    // source sets instead of the required 'commonTest'.
+    commonResourcesDir.set(project.projectDir.resolve("src").resolve("commonTest").resolve("resources"))
     destination.set(layout.buildDirectory.dir("generated-code-for-resources/src/commonTest/kotlin"))
 }

674bfb782460449f519966a71ddff0b893cfc062
Piotr Krzeminski <git@krzeminski.it>
2025-01-03 11:03:29 +0100

    Commonize DumpAnchorTest
    Part of https://github.com/krzema12/snakeyaml-engine-kmp/issues/269.

---
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/StringOutputStream.kt
@@ -0,0 +1,20 @@
+package it.krzeminski.snakeyaml.engine.kmp
+
+import it.krzeminski.snakeyaml.engine.kmp.api.StreamDataWriter
+
+/**
+ * A convenient utility used to assert on certain output.
+ */
+internal class StringOutputStream(
+    private val builder: StringBuilder = StringBuilder(),
+) : StreamDataWriter {
+    override fun write(str: String) {
+        builder.append(str)
+    }
+
+    override fun write(str: String, off: Int, len: Int) {
+        builder.append(str, off, len)
+    }
+
+    override fun toString(): String = builder.toString()
+}

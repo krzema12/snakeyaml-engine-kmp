22b2c5ae5976efb192a3db91c1afa0b7ce3fa61f
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-01-05 19:31:13 +0100

    Update system.js.kt

--- src/jsCommonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/internal/system.js.kt
+++ src/jsCommonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/internal/system.js.kt
@@ -1,6 +1,5 @@
 package it.krzeminski.snakeyaml.engine.kmp.internal
 
 internal actual fun getEnvironmentVariable(key: String): String? {
-    // TODO how can environment variables be implemented in Kotlin/JS?
-    return null
+    return process.env[key] as String?
 }

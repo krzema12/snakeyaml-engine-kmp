9ca78d4ab93f896a4f170ccd94c823c539eb3e69
Piotr Krzeminski <git@krzeminski.it>
2025-01-05 23:44:46 +0100

    Move EnvVariableJvmTest to new package

--- src/jvmTest/java/it/krzeminski/snakeyaml/engine/kmp/usecases/env/CustomEnvConfig.java
+++ src/jvmTest/java/it/krzeminski/snakeyaml/engine/kmp/usecases/env/CustomEnvConfig.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.usecases.env;
+package it.krzeminski.snakeyaml.engine.kmp.usecases.env;
 
 import org.jetbrains.annotations.NotNull;
 import it.krzeminski.snakeyaml.engine.kmp.env.EnvConfig;
--- src/jvmTest/java/it/krzeminski/snakeyaml/engine/kmp/usecases/env/EnvVariableJvmTest.kt
+++ src/jvmTest/java/it/krzeminski/snakeyaml/engine/kmp/usecases/env/EnvVariableJvmTest.kt
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.snakeyaml.engine.usecases.env
+package it.krzeminski.snakeyaml.engine.kmp.usecases.env
 
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.FunSpec
@@ -34,7 +34,11 @@ class EnvVariableJvmTest : FunSpec({
         val provided = mapOf(KEY1 to "VVVAAA111")
         System.setProperty(EMPTY, "VVVAAA222")
         val loader = Load(
-            builder().setEnvConfig(CustomEnvConfig(provided)).build()
+            builder().setEnvConfig(
+                CustomEnvConfig(
+                    provided
+                )
+            ).build()
         )
         val resource = stringFromResources("env/docker-compose.yaml")
         @Suppress("UNCHECKED_CAST")

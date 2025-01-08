6e51ba5e3514c1548f8612673531ae246caf9760
Piotr Krzeminski <git@krzeminski.it>
2025-01-08 15:16:23 +0100

    Add comment

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/internal/TargetUtils.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/internal/TargetUtils.kt
@@ -1,3 +1,7 @@
 package it.krzeminski.snakeyaml.engine.kmp.internal
 
+/**
+ * Not all targets support environment variables. Thanks to this function being implemented appropriately by each
+ * target, we can e.g. skip tests that rely on the environment variables.
+ */
 expect fun areEnvVarsSupported(): Boolean

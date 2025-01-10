af72b6392a10057bf4c4d8f5ea560cbe4313b787
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-01-10 17:24:00 +0100

    Allow larger common test resources (#355)
    Part of https://github.com/krzema12/snakeyaml-engine-kmp/issues/354
    
    Before this change, the build logic took all test resources, and
    converted them into Kotlin code that was building a map using a single
    expression with using nested `mapOf` function to represent the directory
    structure. However, when trying to move existing JVM-specific resources
    (there's a lot of them), one could see a failing build with:
    
    ```
    exception: java.lang.RuntimeException: Error generating class file CommonTestResources.class (compiled from [/Users/piotr/repos/snakeyaml-engine-kmp/build/generated-code-for-resources/src/commonTest/kotlin/CommonTestResources.kt]): Method too large: CommonTestResources.<clinit> ()V
    e
    ```
    
    This change puts every call to `mapOf(...)` or `ByteString.of(...)` into
    a separate method, so that now the limitation is either the maximum
    number of files/directories in a given directory, or size of a single
    file. It should unblock migrating tests to the common source set that
    rely on the large number of files (see a work-in-progress PR:
    https://github.com/krzema12/snakeyaml-engine-kmp/pull/356).
    
    It was still faster to not use KotlinPoet, but the generated code became
    even more ugly. It's likely that I'm going to use KotlinPoet, especially
    that I'm planning to extract this functionality into a library.

--- buildSrc/src/main/kotlin/buildsrc/tasks/ConvertCommonTestResourcesToKotlin.kt
+++ buildSrc/src/main/kotlin/buildsrc/tasks/ConvertCommonTestResourcesToKotlin.kt
@@ -59,29 +59,50 @@ abstract class ConvertCommonTestResourcesToKotlin @Inject constructor(
         return resourcesMap
     }
 
-    private fun generateKotlinCode(resourcesMap: Map<String, Any>): String =
-        """
+    private fun generateKotlinCode(resourcesMap: Map<String, Any>): String {
+        val stringBuilder = StringBuilder()
+        generateFunctions(resourcesMap, stringBuilder)
+        return """
             import okio.ByteString
 
             object CommonTestResources {
-                val resourcesMap: Map<String, Any> = ${generateMapCode(resourcesMap)}
+                val resourcesMap: Map<String, Any> = ${getFunctionName("")}()
+
+                $stringBuilder
             }
         """.trimIndent()
+    }
 
-    private fun generateMapCode(map: Map<String, Any>): String {
-        return map.entries.joinToString(
-            separator = ",\n",
-            prefix = "mapOf(\n",
-            postfix = "\n)",
-        ) { (key, value) ->
-            @Suppress("UNCHECKED_CAST")
+    private fun generateFunctions(map: Map<String, Any>, stringBuilder: StringBuilder, path: String = "") {
+        stringBuilder.generateSingleFunction(map, path)
+        stringBuilder.appendLine()
+        for ((key, value) in map) {
             when (value) {
-                is ByteArray -> "\"$key\" to ByteString.of(${value.joinToString(separator = ", ") {
+                is ByteArray -> stringBuilder.append("fun ${getFunctionName("$path/$key")}() = ByteString.of(${value.joinToString(separator = ", ") {
                     "0x${"%02x".format(it)}.toByte()"
-                }})"
-                is Map<*, *> -> "\"$key\" to ${generateMapCode(value as Map<String, Any>)}"
-                else -> error("Unexpected type: $value")
+                }})")
+                is Map<*, *> -> {
+                    @Suppress("UNCHECKED_CAST")
+                    generateFunctions(map[key] as Map<String, Any>, stringBuilder, "$path/$key")
+                }
             }
         }
     }
+
+    private fun StringBuilder.generateSingleFunction(map: Map<String, Any>, path: String) {
+        val functionName = getFunctionName(path)
+        map.entries.joinTo(
+            this,
+            separator = ",\n",
+            prefix = "fun ${functionName}() = mapOf(\n",
+            postfix = "\n)\n",
+        ) { (key, _) ->
+            "\"$key\" to ${getFunctionName("$path/$key")}()"
+        }
+    }
+
+    private val pathToFunctionName: MutableMap<String, String> = mutableMapOf()
+
+    private fun getFunctionName(path: String): String =
+        pathToFunctionName.getOrPut(path) { "function${pathToFunctionName.size}" }
 }

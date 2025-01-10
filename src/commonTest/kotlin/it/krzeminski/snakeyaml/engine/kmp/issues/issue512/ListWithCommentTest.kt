385c9002f7fbe66fdbc12679d2ae87201eb7e3fb
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-01-10 10:32:47 +0100

    Kotlinize and commonize ListWithCommentTest (#353)
    Part of https://github.com/krzema12/snakeyaml-engine-kmp/issues/269

---
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue512/ListWithCommentTest.kt
@@ -0,0 +1,18 @@
+package it.krzeminski.snakeyaml.engine.kmp.issues.issue512
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import it.krzeminski.snakeyaml.engine.kmp.api.Load
+import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
+import it.krzeminski.snakeyaml.engine.kmp.stringFromResources
+
+class ListWithCommentTest: FunSpec({
+    test("Issue 512 from SnakeYAML") {
+        val str = stringFromResources("/comments/issue512.yaml")
+        val options = LoadSettings.builder().setParseComments(true).build()
+        val load = Load(options)
+        @Suppress("UNCHECKED_CAST")
+        val obj = load.loadOne(str) as List<String>
+        obj.size shouldBe 2
+    }
+})

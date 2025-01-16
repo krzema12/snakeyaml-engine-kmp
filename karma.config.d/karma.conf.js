a19deb57738916b442b625f95422194a1171d048
Oleg Smirnov <oleg31101996@gmail.com>
2025-01-16 20:58:22 +0400

    Increase captureTimeout in karma config

--- karma.config.d/karma.conf.js
+++ karma.config.d/karma.conf.js
@@ -1,3 +1,12 @@
+module.exports = function(config) {
+    config.set({
+        // https://karma-runner.github.io/6.4/config/configuration-file.html#capturetimeout
+        // default timeout 60_000, but looks like it is not enough for macOS on GitHub runner
+        // increasing the timeout should help to reduce number of failures
+        captureTimeout: 120_000,
+    });
+};
+
 // A workaround from https://android-review.googlesource.com/c/platform/frameworks/support/+/3413540
 (function() {
     const originalExit = process.exit;

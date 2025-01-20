a447f091a9f27639dbb3f4b2bd11d49adf411329
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-01-20 13:45:49 +0100

    Reuse common resource access infra for YAML test suite (#372)
    Thanks to this change, we reuse what was built to access common
    resources to also access YAML test suite data, allowing us to simplify
    the build logic and have a more uniform way of accessing both kinds of
    common resources.

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/test_suite/YamlTestData.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/test_suite/YamlTestData.kt
@@ -1,5 +1,8 @@
 package it.krzeminski.snakeyaml.engine.kmp.test_suite
 
+import it.krzeminski.snakeyaml.engine.kmp.buildFileSystem
+import it.krzeminski.snakeyaml.engine.kmp.internal.FSPath
+import it.krzeminski.snakeyaml.engine.kmp.internal.fsPath
 import kotlin.jvm.JvmInline
 
 
@@ -29,5 +32,40 @@ internal sealed interface YamlTestData {
 
     interface Success : YamlTestData {
         override val testEvent: String
+    }
+}
+
+val YamlTestResourcesFileSystem = buildFileSystem(YamlTestSuiteResources.resourcesMap)
+
+internal val YamlTestSuiteData: Map<YamlTestData.Id, YamlTestData> =
+    YamlTestResourcesFileSystem.fsPath("/").listRecursively()
+        .filter { it.isDirectory && (it / "===").exists }
+        .associate { dir ->
+            val id = YamlTestData.Id(dir.path.segments.joinToString(":"))
+            id to generateYamlTestDataObject(id, dir)
+        }
+
+private fun generateYamlTestDataObject(
+    id: YamlTestData.Id,
+    path: FSPath,
+): YamlTestData {
+    val isError = (path / "error").exists
+    return if (isError) {
+        object : YamlTestData.Error {
+            override val id: YamlTestData.Id = id
+            override val label: String = (path / "===").readUtf8()
+            override val inYaml: String = (path / "in.yaml").readUtf8()
+
+        }
+    } else {
+        object : YamlTestData.Success {
+            override val id: YamlTestData.Id = id
+            override val label: String = (path / "===").readUtf8()
+            override val inYaml: String = (path / "in.yaml").readUtf8()
+            override val testEvent: String = (path / "test.event").readUtf8()
+            override val outYaml: String? = (path / "out.yaml").readUtf8OrNull()
+            override val emitYaml: String? = (path / "emit.yaml").readUtf8OrNull()
+            override val inJson: String? = (path / "in.json").readUtf8OrNull()
+        }
     }
 }

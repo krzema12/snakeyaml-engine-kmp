f00316e7e10e27de9d11a95297f516572acabf5c
Oleg Smirnov <oleg31101996@gmail.com>
2025-01-28 22:07:36 +0400

    Correct grammar in comments. Add additional description for methods

--- src/commonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/internal/utils/BufferedSourceExtensions.kt
+++ src/commonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/internal/utils/BufferedSourceExtensions.kt
@@ -2,8 +2,11 @@ package it.krzeminski.snakeyaml.engine.kmp.internal.utils
 
 import okio.BufferedSource
 
-@Suppress("NOTHING_TO_INLINE")
+/**
+ * @return a well-formed (if possible) UTF8 string with a size at most [limitBytes]
+ */
 // based on benchmarks it looks like having inline here actually makes a small difference in the performance
+@Suppress("NOTHING_TO_INLINE")
 internal inline fun BufferedSource.readUtf8WithLimit(limitBytes: Long): String {
     val fullUtf8Size = sizeOfFullValidUtf8String(limitBytes)
     val buffer = readUtf8(fullUtf8Size)
@@ -29,43 +32,44 @@ private inline fun BufferedSource.sizeOfFullValidUtf8String(limitBytes: Long): L
         return originalSize - if (byte < 0) 1 else 0
     }
 
-    // check if the source has one more byte in it
+    // Check if the source has one more byte in it
     if (!request(originalSize + 1)) {
-        // no more bytes left in the source, return the size
+        // no more bytes left in the source, return the original size
         return originalSize
     }
 
-    // check the next byte
-    // if it is not a continuation byte then bytes up to that point form a full UTF8 string
-    // or at least the last character is complete, and we don't split it in half
+    // Check the next byte.
+    // If it is not a continuation byte then bytes up to that point form a full "valid" UTF8 string
+    // (or at least the last character is complete, and we don't split it in half)
     if (!isContinuationByte(buffer[originalSize])) {
         return originalSize
     }
 
     var size = originalSize
-    // A valid codepoint consist of 1 leading byte and at most 3 continuation bytes (0-3)
+    // A valid codepoint consists of 1 leading byte and at most 3 continuation bytes (0-3)
     // https://www.rfc-editor.org/rfc/rfc3629.html#section-3
-    // We go backwards to find the start of the codepoint at which we stopped
+    // We go backwards to find the start of the codepoint at which we have stopped.
     // The marker for that is a non-continuation byte
     repeat(3) {
         size -= 1
         if (size == 0L) {
-            // no more data left in the buffer
-            // this means we have an invalid UTF8 string
-            // however, the Okio library will handle this and replace invalid codepoints with replacement codepoint
+            // No more data left in the buffer.
+            // This means we have an ill-formed UTF8 string.
+            // However, the Okio library will handle this and replace invalid codepoints with replacement codepoint
             return originalSize
         }
         val byte = buffer[size - 1]
         if (!isContinuationByte(byte)) {
-            // we are on the marker
-            // it might be an ASCII (byte > 0) or Unicode (byte < 0)
-            // the first case is probably invalid since we have continuation bytes after ASCII codepoint
+            // We are on the marker.
+            // It might be an ASCII (byte > 0) or Unicode (byte < 0).
+            // The first case is probably invalid since we have continuation bytes after ASCII codepoint
             // but this will be handled by Okio.
             // We just need to return the correct position in bytes of the last full codepoint
             return when {
+                // ASCII codepoint
                 byte >= 0                                -> originalSize
                 // this can happen if we start from an ill-formed byte and go over the valid UTF8 codepoint
-                // that has less continuation bytes then we read
+                // that has less continuation bytes than we read
                 continuationBytesCountFor(byte) < it + 1 -> originalSize
 
                 else                                     -> size - 1
@@ -73,21 +77,23 @@ private inline fun BufferedSource.sizeOfFullValidUtf8String(limitBytes: Long): L
         }
     }
 
-    // this can only happen if the edge bytes (n-3,n-2,n-1) are all continuation
-    // that would mean the UTF8 string is ill-formed
-    // but, as before, this will be handled by Okio
+    // This can only happen if the edge bytes (n-3,n-2,n-1) are all continuation bytes.
+    // That would mean the UTF8 string is ill-formed but, as before, this will be handled by Okio
     return originalSize
 }
 
 @Suppress("NOTHING_TO_INLINE")
 private inline fun isContinuationByte(byte: Byte): Boolean {
-    var asInt = byte.toInt()
+    val asInt = byte.toInt()
     return asInt and 0xc0 == 0x80
 }
 
+/**
+ * @return the number of bytes that must follow [byte] to form a well-formed UTF8 codepoint
+ */
 @Suppress("NOTHING_TO_INLINE")
 private inline fun continuationBytesCountFor(byte: Byte): Int {
-    var asInt = byte.toInt()
+    val asInt = byte.toInt()
 
     return when {
         // 110xxxxx

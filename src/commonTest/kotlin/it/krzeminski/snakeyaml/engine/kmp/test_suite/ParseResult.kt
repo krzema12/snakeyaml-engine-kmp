434ac1feae15a37b3d73eaf04feb3e28eb204427
Björn Kautler <Bjoern@Kautler.net>
2025-02-11 12:10:39 +0100

    Verify emitted events also for error test suite tests (#392)

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/test_suite/ParseResult.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/test_suite/ParseResult.kt
@@ -3,6 +3,6 @@ package it.krzeminski.snakeyaml.engine.kmp.test_suite
 import it.krzeminski.snakeyaml.engine.kmp.events.Event
 
 internal data class ParseResult(
-    val events: List<Event>?,
+    val events: List<Event>,
     val error: Throwable? = null,
 )

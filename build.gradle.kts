40fe67116c76175e91e0b2e98037ac7ecdec3513
Piotr Krzeminski <git@krzeminski.it>
2025-07-31 14:15:03 +0200

    Remove coroutines version override

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,10 +39,6 @@ kotlin {
                 implementation(libs.kotest.framework.engine)
                 implementation(libs.kotest.assertions.core)
                 implementation("org.jetbrains:annotations:26.0.2")
-                // Overridig coroutines' version to solve a problem with WASM JS tests.
-                // See https://kotlinlang.slack.com/archives/CDFP59223/p1736191408326039?thread_ts=1734964013.996149&cid=CDFP59223
-                // TODO: remove this workaround in https://github.com/krzema12/snakeyaml-engine-kmp/issues/337
-                runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
             }
         }
 

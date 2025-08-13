59d910315fbaa07adb66a737bb8316368115fb9a
Piotr Krzeminski <git@krzeminski.it>
2025-08-13 08:41:29 +0200

    [Port from snakeyaml-engine] Refine tests for dividing strings with spaces
    Fixes https://github.com/krzema12/snakeyaml-engine-kmp/issues/506.
    
    Ports:
     * https://bitbucket.org/snakeyaml/snakeyaml-engine/commits/7eea790d14c986276f0095dd87694d5510cb34fd
     * https://bitbucket.org/snakeyaml/snakeyaml-engine/commits/061a977e9830bbd300e3a3c0cddf553a2ff4275c
     * https://bitbucket.org/snakeyaml/snakeyaml-engine/commits/9dfb6b6a1a45f4802f4ae3de5b22353cdc101724
     * https://bitbucket.org/snakeyaml/snakeyaml-engine/commits/e55f1a6957d5f422f62dd61260ca68a2ac9a01a8

---
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue47/SetWidthTest.kt
@@ -0,0 +1,78 @@
+package it.krzeminski.snakeyaml.engine.kmp.issues.issue47
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import it.krzeminski.snakeyaml.engine.kmp.api.Dump
+import it.krzeminski.snakeyaml.engine.kmp.api.DumpSettings
+import it.krzeminski.snakeyaml.engine.kmp.api.Load
+import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
+import it.krzeminski.snakeyaml.engine.kmp.api.StringStreamDataWriter
+import it.krzeminski.snakeyaml.engine.kmp.common.ScalarStyle
+import it.krzeminski.snakeyaml.engine.kmp.emitter.Emitter
+import it.krzeminski.snakeyaml.engine.kmp.events.*
+
+class SetWidthTest : FunSpec({
+    val stringToSerialize = "arn:aws:iam::12345678901234567890:foobarbaz:testing:testing2:role/github-actions-role/\${{ github.token }}"
+
+    test("emit plain string and split") {
+        val settings = DumpSettings.builder().setWidth(80).build() // Intentionally limited.
+        val writer = StringStreamDataWriter()
+        val dump = Dump(settings)
+        dump.dump(stringToSerialize, writer)
+        val yaml = writer.toString()
+        val expected = "arn:aws:iam::12345678901234567890:foobarbaz:testing:testing2:role/github-actions-role/\${{\n  github.token }}"
+        parseBack(yaml) shouldBe stringToSerialize
+        yaml.trim() shouldBe expected
+    }
+
+    test("emit plain and split") {
+        val settings = DumpSettings.builder().setWidth(80).build() // Intentionally limited.
+        val writer = StringStreamDataWriter()
+        with(Emitter(settings, writer)) {
+            emit(StreamStartEvent())
+            emit(DocumentStartEvent(false, null, emptyMap()))
+            emit(ScalarEvent(null, null, ImplicitTuple(true, true), stringToSerialize, ScalarStyle.PLAIN))
+            emit(DocumentEndEvent(false))
+            emit(StreamEndEvent())
+        }
+        val yaml = writer.toString()
+        val expected = "arn:aws:iam::12345678901234567890:foobarbaz:testing:testing2:role/github-actions-role/\${{\n  github.token }}\n"
+        yaml shouldBe expected
+    }
+
+    test("emit plain and no split") {
+        val settings = DumpSettings.builder().setWidth(180).build() // Intentionally limited.
+        val writer = StringStreamDataWriter()
+        with(Emitter(settings, writer)) {
+            emit(StreamStartEvent())
+            emit(DocumentStartEvent(false, null, emptyMap()))
+            emit(ScalarEvent(null, null, ImplicitTuple(true, true), stringToSerialize, ScalarStyle.PLAIN))
+            emit(DocumentEndEvent(false))
+            emit(StreamEndEvent())
+        }
+        val yaml = writer.toString()
+        val expected = "arn:aws:iam::12345678901234567890:foobarbaz:testing:testing2:role/github-actions-role/\${{ github.token }}\n"
+        yaml shouldBe expected
+        parseBack(yaml) shouldBe stringToSerialize
+    }
+
+    test("emit folded") {
+        val settings = DumpSettings.builder()
+            .setWidth(80) // Intentionally limited.
+            .setDefaultScalarStyle(ScalarStyle.FOLDED)
+            .build()
+        val dump = Dump(settings)
+        val yaml = dump.dumpToString(stringToSerialize)
+        val expected = ">-\n" +
+            "  arn:aws:iam::12345678901234567890:foobarbaz:testing:testing2:role/github-actions-role/\${{\n" +
+            "  github.token }}\n"
+        yaml shouldBe expected
+        parseBack(yaml) shouldBe stringToSerialize
+    }
+})
+
+private fun parseBack(yaml: String): String {
+    val settings = LoadSettings.builder().build()
+    val load = Load(settings)
+    return load.loadOne(yaml).toString()
+}

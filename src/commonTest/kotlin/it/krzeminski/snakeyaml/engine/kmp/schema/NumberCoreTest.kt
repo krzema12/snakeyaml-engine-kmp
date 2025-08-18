0799f55eadbbd1624024c290e9e7b403f4677498
Piotr Krzeminski <git@krzeminski.it>
2025-08-18 08:18:36 +0200

    Migrate other test cases

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/schema/NumberCoreTest.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/schema/NumberCoreTest.kt
@@ -6,6 +6,8 @@ import io.kotest.data.headers
 import io.kotest.data.row
 import io.kotest.data.table
 import io.kotest.matchers.shouldBe
+import it.krzeminski.snakeyaml.engine.kmp.api.Dump
+import it.krzeminski.snakeyaml.engine.kmp.api.DumpSettings
 import it.krzeminski.snakeyaml.engine.kmp.api.Load
 import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
 
@@ -75,14 +77,60 @@ class NumberCoreTest : FunSpec({
     }
 
     test("all doubles which are defined in the core schema & JSON") {
-
+        forAll(
+            table(
+                headers("string", "value"),
+                row("-1.345", -1.345),
+                row("0.0", 0.0),
+                row("-0.0", 0.0),
+                row("0.123", 0.123),
+                row("1.23e-6", 1.23E-6),
+                row("1.23e+6", 1.23E6),
+                row("1.23e6", 1.23E6),
+                row("1.23E6", 1.23E6),
+                row("-1.23e6", -1.23E6),
+                row("1000.25", 1000.25),
+                row("9000.0", 9000.0),
+                row("1.", 1.0),
+            )
+        ) { string: String, value: Any ->
+            loader.loadOne(string) shouldBe value
+        }
     }
 
     test("parse special doubles which are defined in the core schema") {
-
+        forAll(
+            table(
+                headers("string", "value"),
+                row(".inf", Double.POSITIVE_INFINITY),
+                row(".Inf", Double.POSITIVE_INFINITY),
+                row(".INF", Double),
+                row(".-inf", Double.NEGATIVE_INFINITY),
+                row(".-Inf", Double.NEGATIVE_INFINITY),
+                row(".-INF", Double.NEGATIVE_INFINITY),
+                row(".nan", Double.NaN),
+                row(".NaN", Double.NaN),
+                row(".NAN", Double.NaN),
+            )
+        ) { string: String, value: Any ->
+            loader.loadOne(string) shouldBe value
+        }
     }
 
     test("dump special doubles which are defined in the core schema") {
-
+        val dumper = Dump(DumpSettings.builder().setSchema(CoreSchema()).build());
+        forAll(
+            table(
+                headers("value", "string"),
+                row(Double.POSITIVE_INFINITY, ".inf\n"),
+                row(Float.POSITIVE_INFINITY, ".inf\n"),
+                row(Double.NEGATIVE_INFINITY, "-.inf\n"),
+                row(Float.NEGATIVE_INFINITY, "-.inf\n"),
+                row(Double.NaN, ".nan\n"),
+                row(Float.NaN, ".nan\n"),
+            )
+        ) { value: Any, string: String ->
+            dumper.dumpToString(value) shouldBe string
+        }
     }
 })

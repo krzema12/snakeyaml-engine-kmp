33a1882135ce96040a3c77b0552b9a7fb76d6e74
Piotr Krzeminski <git@krzeminski.it>
2025-08-19 12:40:38 +0200

    Share DumpSettingsBuilder creation, like in original test

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/api/dump/DumpWidthTest.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/api/dump/DumpWidthTest.kt
@@ -8,11 +8,14 @@ import it.krzeminski.snakeyaml.engine.kmp.api.Load
 import it.krzeminski.snakeyaml.engine.kmp.common.ScalarStyle
 
 class DumpWidthTest : FunSpec({
+    fun dumpSettingWithSplit() = DumpSettings.builder().setSplitLines(true)
+    fun dumpSettingWithoutSplit() = DumpSettings.builder().setSplitLines(false)
+
     val data1 = "1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888 9999999999 0000000000"
     val data2 = "1111111111  2222222222  3333333333  4444444444  5555555555  6666666666  7777777777  8888888888  9999999999  0000000000"
 
     test("split lines double quoted") {
-        val dump = Dump(DumpSettings.builder().setSplitLines(true).setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED).build())
+        val dump = Dump(dumpSettingWithSplit().setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED).build())
         // Split lines enabled (default)
         var output = dump.dumpToString(data1)
         output shouldBe "\"1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888\\\n  \\ 9999999999 0000000000\"\n"
@@ -22,13 +25,13 @@ class DumpWidthTest : FunSpec({
         output shouldBe "\"1111111111  2222222222  3333333333  4444444444  5555555555  6666666666  7777777777\\\n  \\  8888888888  9999999999  0000000000\"\n"
 
         // Split lines disabled
-        val dump2 = Dump(DumpSettings.builder().setSplitLines(false).setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED).build())
+        val dump2 = Dump(dumpSettingWithoutSplit().setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED).build())
 
         output = dump2.dumpToString(data1)
         output shouldBe "\"1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888 9999999999 0000000000\"\n"
 
         // setWidth
-        val dump3 = Dump(DumpSettings.builder().setSplitLines(true).setWidth(15).setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED).build())
+        val dump3 = Dump(dumpSettingWithSplit().setWidth(15).setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED).build())
         output = dump3.dumpToString(data1)
         output shouldBe "\"1111111111 2222222222\\\n" + "  \\ 3333333333 4444444444\\\n" +
                 "  \\ 5555555555 6666666666\\\n" + "  \\ 7777777777 8888888888\\\n" +
@@ -36,7 +39,7 @@ class DumpWidthTest : FunSpec({
     }
 
     test("split lines single quoted") {
-        val dump = Dump(DumpSettings.builder().setSplitLines(true).setDefaultScalarStyle(ScalarStyle.SINGLE_QUOTED).build())
+        val dump = Dump(dumpSettingWithSplit().setDefaultScalarStyle(ScalarStyle.SINGLE_QUOTED).build())
         // Split lines enabled (default)
         var output = dump.dumpToString(data1)
         output shouldBe "'1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888\n  9999999999 0000000000'\n"
@@ -46,14 +49,14 @@ class DumpWidthTest : FunSpec({
         output shouldBe "'1111111111  2222222222  3333333333  4444444444  5555555555  6666666666  7777777777  8888888888  9999999999  0000000000'\n"
 
         // Split lines disabled
-        val dump2 = Dump(DumpSettings.builder().setSplitLines(false).setDefaultScalarStyle(ScalarStyle.SINGLE_QUOTED).build())
+        val dump2 = Dump(dumpSettingWithoutSplit().setDefaultScalarStyle(ScalarStyle.SINGLE_QUOTED).build())
 
         output = dump2.dumpToString(data1)
         output shouldBe "'1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888 9999999999 0000000000'\n"
     }
 
     test("split lines folded") {
-        val dump = Dump(DumpSettings.builder().setSplitLines(true).setDefaultScalarStyle(ScalarStyle.FOLDED).build())
+        val dump = Dump(dumpSettingWithSplit().setDefaultScalarStyle(ScalarStyle.FOLDED).build())
         // Split lines enabled (default)
         var output = dump.dumpToString(data1)
         output shouldBe ">-\n  1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888\n  9999999999 0000000000\n"
@@ -67,21 +70,21 @@ class DumpWidthTest : FunSpec({
         output shouldBe ">-\n  1111111111  2222222222  3333333333  4444444444  5555555555  6666666666  7777777777  8888888888  9999999999  0000000000\n"
 
         // Split lines disabled
-        val dump2 = Dump(DumpSettings.builder().setSplitLines(false).setDefaultScalarStyle(ScalarStyle.FOLDED).build())
+        val dump2 = Dump(dumpSettingWithoutSplit().setDefaultScalarStyle(ScalarStyle.FOLDED).build())
 
         output = dump2.dumpToString(data1)
         output shouldBe ">-\n  1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888 9999999999 0000000000\n"
     }
 
     test("split lines literal") {
-        val dump = Dump(DumpSettings.builder().setSplitLines(true).setDefaultScalarStyle(ScalarStyle.LITERAL).build())
+        val dump = Dump(dumpSettingWithSplit().setDefaultScalarStyle(ScalarStyle.LITERAL).build())
         val output = dump.dumpToString(data1)
         // Split lines enabled (default) -- split lines does not apply to literal style
         output shouldBe "|-\n  1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888 9999999999 0000000000\n"
     }
 
     test("split lines plain") {
-        val dump = Dump(DumpSettings.builder().setSplitLines(true).setDefaultScalarStyle(ScalarStyle.PLAIN).build())
+        val dump = Dump(dumpSettingWithSplit().setDefaultScalarStyle(ScalarStyle.PLAIN).build())
         // Split lines enabled (default)
         var output = dump.dumpToString(data1)
         output shouldBe "1111111111 2222222222 3333333333 4444444444 5555555555 6666666666 7777777777 8888888888\n  9999999999 0000000000\n"
@@ -91,7 +94,7 @@ class DumpWidthTest : FunSpec({
         output shouldBe "$data2\n"
 
         // Split lines disabled
-        val dump2 = Dump(DumpSettings.builder().setSplitLines(false).setDefaultScalarStyle(ScalarStyle.PLAIN).build())
+        val dump2 = Dump(dumpSettingWithoutSplit().setDefaultScalarStyle(ScalarStyle.PLAIN).build())
 
         output = dump2.dumpToString(data1)
         output shouldBe "$data1\n"

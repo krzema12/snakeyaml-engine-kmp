9eb379e19f6b4b65a286580891d6e72f96346f88
Piotr Krzeminski <git@krzeminski.it>
2025-08-19 13:58:38 +0200

    Fix imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ kotlin {
             dependencies {
                 implementation(libs.kotest.framework.engine)
                 implementation(libs.kotest.assertions.core)
+                implementation(libs.kotest.assertions.table)
                 implementation(libs.jetbrains.annotations)
                 // Overridig coroutines' version to solve a problem with WASM JS tests.
                 // See https://kotlinlang.slack.com/archives/CDFP59223/p1736191408326039?thread_ts=1734964013.996149&cid=CDFP59223

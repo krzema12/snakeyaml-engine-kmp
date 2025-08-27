b94df9ab3d4779f9c7961408e9b0b6b31963f5dd
Piotr Krzeminski <git@krzeminski.it>
2025-08-27 14:48:43 +0200

    Fix ExceptionTest on non-JVM

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue3/ExceptionTest.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue3/ExceptionTest.kt
@@ -1,6 +1,8 @@
 package it.krzeminski.snakeyaml.engine.kmp.issues.issue3
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.common.Platform
+import io.kotest.common.platform
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
@@ -13,17 +15,23 @@ class ExceptionTest : FunSpec({
         val exception = shouldThrow<YamlEngineException> {
             load.loadOne("!!seq abc")
         }
-        exception.message shouldContain "java.lang.ClassCastException"
-        exception.message shouldContain "it.krzeminski.snakeyaml.engine.kmp.nodes.ScalarNode"
-        exception.message shouldContain "cannot be cast to"
-        exception.message shouldContain "it.krzeminski.snakeyaml.engine.kmp.nodes.SequenceNode"
+        exception.message shouldContain "ClassCastException"
     }
 
     test("Int exception") {
         val load = Load()
-        val exception = shouldThrow<YamlEngineException> {
-            load.loadOne("!!int abc")
+        val operationUnderTest = { load.loadOne("!!int abc") }
+        if (platform == Platform.JVM) {
+            val exception = shouldThrow<YamlEngineException> {
+                operationUnderTest()
+            }
+            exception.message shouldBe "java.lang.NumberFormatException: For input string: \"abc\""
+        } else {
+            // TODO: https://github.com/krzema12/snakeyaml-engine-kmp/issues/49
+            val exception = shouldThrow<NotImplementedError> {
+                operationUnderTest()
+            }
+            exception.message shouldContain "An operation is not implemented: Kotlin/JS BigInteger implementation"
         }
-        exception.message shouldBe "java.lang.NumberFormatException: For input string: \"abc\""
     }
 })

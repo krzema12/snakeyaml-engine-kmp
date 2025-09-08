9b238577bf9b632865e48661e8f6bbd8dc6a9cd5
Piotr Krzeminski <git@krzeminski.it>
2025-09-08 12:49:27 +0200

    Account for different Regex string representation in JS

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/resolver/ScalarResolverTupleTest.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/resolver/ScalarResolverTupleTest.kt
@@ -1,11 +1,17 @@
 package it.krzeminski.snakeyaml.engine.kmp.resolver
 
+import io.kotest.common.Platform
+import io.kotest.common.platform
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import it.krzeminski.snakeyaml.engine.kmp.nodes.Tag
 
 class ScalarResolverTupleTest : FunSpec({
     test("toString()") {
-        ResolverTuple(Tag.STR, Regex("^(?:true|false)$")).toString() shouldBe "Tuple tag=tag:yaml.org,2002:str regexp=^(?:true|false)$"
+        ResolverTuple(Tag.STR, Regex("^(?:true|false)$")).toString() shouldBe if (platform == Platform.JVM) {
+            "Tuple tag=tag:yaml.org,2002:str regexp=^(?:true|false)$"
+        } else {
+            "Tuple tag=tag:yaml.org,2002:str regexp=/^(?:true|false)$/gu"
+        }
     }
 })

2bceb613cbaf1dc06bf4c428d3c95181838343ab
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-09-14 21:38:09 +0200

    [Port from snakeyaml-engine] Parse escaped TAB in double-quoted scalar (#569)
    Fixes https://github.com/krzema12/snakeyaml-engine-kmp/issues/560.
    
    Ports
    https://bitbucket.org/snakeyaml/snakeyaml-engine/commits/5656f99119c07ed5be6bbf42e7a6f4eadb67a86d.

---
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue69/TabInDoubleQuoteTest.kt
@@ -0,0 +1,18 @@
+package it.krzeminski.snakeyaml.engine.kmp.issues.issue69
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import it.krzeminski.snakeyaml.engine.kmp.api.Load
+import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
+
+class TabInDoubleQuoteTest : FunSpec({
+    test("tab in double quote") {
+        val options = LoadSettings.builder().setParseComments(true).build()
+        val load = Load(options)
+        val str = "- \"\\\t\"" // "\TAB"
+        val obj = load.loadOne(str) as List<String>
+        obj shouldHaveSize 1
+        obj[0] shouldBe "\t"
+    }
+})

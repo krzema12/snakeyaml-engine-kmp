bbff84ccd81f627390b806281fb7d030ebd10fa3
Piotr Krzeminski <git@krzeminski.it>
2025-09-18 12:50:20 +0200

    [Port from snakeyaml-engine] Improve handling comments

---
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue68/CommentAfterScalarTest.kt
@@ -0,0 +1,29 @@
+package it.krzeminski.snakeyaml.engine.kmp.issues.issue68
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
+import it.krzeminski.snakeyaml.engine.kmp.api.lowlevel.Compose
+
+class CommentAfterScalarTest : FunSpec({
+    val loadSettings = LoadSettings.builder().setParseComments(true).build()
+
+    test("respect inline comment for '!!str # comment'") {
+        val compose = Compose(loadSettings)
+        val node = compose.compose("!!str # comment")
+        node.shouldNotBeNull()
+        node.inLineComments?.size shouldBe 1
+        node.inLineComments?.first()?.value shouldBe " comment"
+    }
+
+    test("respect inline and block comments for '!!str # comment\\n# block comment1'") {
+        val compose = Compose(loadSettings)
+        val node = compose.compose("!!str # comment\n# block comment1\n# block comment2")
+        node.shouldNotBeNull()
+        node.inLineComments?.size shouldBe 1
+        node.inLineComments?.map { it.value } shouldBe listOf(" comment")
+        node.blockComments?.size shouldBe 2
+        node.blockComments?.map { it.value } shouldBe listOf(" block comment1", " block comment2")
+    }
+})

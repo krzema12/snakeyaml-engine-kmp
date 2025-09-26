bba4410911bb215cb9c1456a52ef438e1a4335d4
Piotr Krzeminski <git@krzeminski.it>
2025-09-26 07:46:05 +0200

    Add tests for failing parsing comment after alias
    These tests present certain undesired behavior that was reported in
    https://github.com/krzema12/snakeyaml-engine-kmp/issues/478 but ultimately
    wasn't fixed in the upstream project.

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue68/CommentAfterAliasTest.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue68/CommentAfterAliasTest.kt
@@ -1,39 +1,95 @@
 package it.krzeminski.snakeyaml.engine.kmp.issues.issue68
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.string.shouldContain
 import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
 import it.krzeminski.snakeyaml.engine.kmp.api.lowlevel.Compose
+import it.krzeminski.snakeyaml.engine.kmp.exceptions.ParserException
 
 class CommentAfterAliasTest : FunSpec({
-    val loadSettings = LoadSettings.builder().setParseComments(false).build()
+    context("parsing comments is disabled") {
+        val loadSettings = LoadSettings.builder().setParseComments(false).build()
 
-    test("inline") {
-        val compose = Compose(loadSettings)
-        val input = """
+        test("inline") {
+            val compose = Compose(loadSettings)
+            val input = """
             |field_with_alias: &alias_name # inline comment 1
             |  555""".trimMargin()
-        val node = compose.compose(input)
-        node.shouldNotBeNull()
-    }
+            val node = compose.compose(input)
+            node.shouldNotBeNull()
+        }
 
-    test("block comment and flat after") {
-        val compose = Compose(loadSettings)
-        val input = """
+        test("block comment and flat after") {
+            val compose = Compose(loadSettings)
+            val input = """
             |field_with_alias: &alias_name
             |# separate line comment following the alias
             |    555""".trimMargin()
-        val node = compose.compose(input)
-        node.shouldNotBeNull()
-    }
+            val node = compose.compose(input)
+            node.shouldNotBeNull()
+        }
 
-    test("block comment and nested after") {
-        val compose = Compose(loadSettings)
-        val input = """
+        test("block comment and nested after") {
+            val compose = Compose(loadSettings)
+            val input = """
             |field_with_alias: &alias_name
             |# separate line comment following the alias
             |    nested_field: nested_value""".trimMargin()
-        val node = compose.compose(input)
-        node.shouldNotBeNull()
+            val node = compose.compose(input)
+            node.shouldNotBeNull()
+        }
+    }
+
+    context("parsing comments is enabled") {
+        val loadSettings = LoadSettings.builder().setParseComments(true).build()
+
+        test("inline") {
+            val compose = Compose(loadSettings)
+            val input = """
+            |field_with_alias: &alias_name # inline comment 1
+            |  555""".trimMargin()
+            // This test shows a certain undesired behavior that ideally should be fixed one day.
+            // See https://github.com/krzema12/snakeyaml-engine-kmp/issues/478.
+            shouldThrow<ParserException> {
+                compose.compose(input)
+            }.also {
+                it.message shouldContain "while parsing a block mapping"
+                it.message shouldContain "expected <block end>, but found '<scalar>'"
+            }
+        }
+
+        test("block comment and flat after") {
+            val compose = Compose(loadSettings)
+            val input = """
+            |field_with_alias: &alias_name
+            |# separate line comment following the alias
+            |    555""".trimMargin()
+            // This test shows a certain undesired behavior that ideally should be fixed one day.
+            // See https://github.com/krzema12/snakeyaml-engine-kmp/issues/478.
+            shouldThrow<ParserException> {
+                compose.compose(input)
+            }.also {
+                it.message shouldContain "while parsing a block mapping"
+                it.message shouldContain "expected <block end>, but found '<scalar>'"
+            }
+        }
+
+        test("block comment and nested after") {
+            val compose = Compose(loadSettings)
+            val input = """
+            |field_with_alias: &alias_name
+            |# separate line comment following the alias
+            |    nested_field: nested_value""".trimMargin()
+            // This test shows a certain undesired behavior that ideally should be fixed one day.
+            // See https://github.com/krzema12/snakeyaml-engine-kmp/issues/478.
+            shouldThrow<ParserException> {
+                compose.compose(input)
+            }.also {
+                it.message shouldContain "while parsing a block mapping"
+                it.message shouldContain "expected <block end>, but found '<block mapping start>'"
+            }
+        }
     }
 })

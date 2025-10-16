b4f25f8275c2472c2e090a7f4b183e65c7422dfb
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-10-16 22:33:51 +0200

    Add `copy` methods to DumpSettings and LoadSettings (#592)
    It's a follow-up to this discussion:
    https://github.com/krzema12/snakeyaml-engine-kmp/pull/589#discussion_r2421789323,
    related to removing Java-style builders.
    
    The `copy { }` methods are fine wrt. binary compatibility. As we add
    more fields, it should be possible to preserve binary compatibility.

--- src/commonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/api/DumpSettings.kt
+++ src/commonMain/kotlin/it/krzeminski/snakeyaml/engine/kmp/api/DumpSettings.kt
@@ -185,4 +185,84 @@ class DumpSettings(
             throw EmitterException("Indent must be at in range ${Emitter.VALID_INDENT_RANGE}")
         }
     }
+
+    fun copy(modifications: MutableDumpSettings.() -> Unit): DumpSettings {
+        val result = object : MutableDumpSettings {
+            override var isExplicitStart = this@DumpSettings.isExplicitStart
+            override var isExplicitEnd = this@DumpSettings.isExplicitEnd
+            override var explicitRootTag = this@DumpSettings.explicitRootTag
+            override var anchorGenerator = this@DumpSettings.anchorGenerator
+            override var yamlDirective = this@DumpSettings.yamlDirective
+            override var tagDirective = this@DumpSettings.tagDirective
+            override var defaultFlowStyle = this@DumpSettings.defaultFlowStyle
+            override var defaultScalarStyle = this@DumpSettings.defaultScalarStyle
+            override var nonPrintableStyle = this@DumpSettings.nonPrintableStyle
+            override var schema = this@DumpSettings.schema
+            override var isCanonical = this@DumpSettings.isCanonical
+            override var isMultiLineFlow = this@DumpSettings.isMultiLineFlow
+            override var isUseUnicodeEncoding = this@DumpSettings.isUseUnicodeEncoding
+            override var indent = this@DumpSettings.indent
+            override var indicatorIndent = this@DumpSettings.indicatorIndent
+            override var width = this@DumpSettings.width
+            override var bestLineBreak = this@DumpSettings.bestLineBreak
+            override var isSplitLines = this@DumpSettings.isSplitLines
+            override var maxSimpleKeyLength = this@DumpSettings.maxSimpleKeyLength
+            override var customProperties = this@DumpSettings.customProperties
+            override var indentWithIndicator = this@DumpSettings.indentWithIndicator
+            override var dumpComments = this@DumpSettings.dumpComments
+            override var isDereferenceAliases = this@DumpSettings.isDereferenceAliases
+        }.apply(modifications)
+
+        return DumpSettings(
+            isExplicitStart = result.isExplicitStart,
+            isExplicitEnd = result.isExplicitEnd,
+            explicitRootTag = result.explicitRootTag,
+            anchorGenerator = result.anchorGenerator,
+            yamlDirective = result.yamlDirective,
+            tagDirective = result.tagDirective,
+            defaultFlowStyle = result.defaultFlowStyle,
+            defaultScalarStyle = result.defaultScalarStyle,
+            nonPrintableStyle = result.nonPrintableStyle,
+            schema = result.schema,
+            isCanonical = result.isCanonical,
+            isMultiLineFlow = result.isMultiLineFlow,
+            isUseUnicodeEncoding = result.isUseUnicodeEncoding,
+            indent = result.indent,
+            indicatorIndent = result.indicatorIndent,
+            width = result.width,
+            bestLineBreak = result.bestLineBreak,
+            isSplitLines = result.isSplitLines,
+            maxSimpleKeyLength = result.maxSimpleKeyLength,
+            customProperties = result.customProperties,
+            indentWithIndicator = result.indentWithIndicator,
+            dumpComments = result.dumpComments,
+            isDereferenceAliases = result.isDereferenceAliases,
+        )
+    }
+
+    interface MutableDumpSettings {
+        var isExplicitStart: Boolean
+        var isExplicitEnd: Boolean
+        var explicitRootTag: Tag?
+        var anchorGenerator: AnchorGenerator
+        var yamlDirective: SpecVersion?
+        var tagDirective: Map<String, String>
+        var defaultFlowStyle: FlowStyle
+        var defaultScalarStyle: ScalarStyle
+        var nonPrintableStyle: NonPrintableStyle
+        var schema: Schema
+        var isCanonical: Boolean
+        var isMultiLineFlow: Boolean
+        var isUseUnicodeEncoding: Boolean
+        var indent: Int
+        var indicatorIndent: Int
+        var width: Int
+        var bestLineBreak: String
+        var isSplitLines: Boolean
+        var maxSimpleKeyLength: Int
+        var customProperties: Map<SettingKey, Any>
+        var indentWithIndicator: Boolean
+        var dumpComments: Boolean
+        var isDereferenceAliases: Boolean
+    }
 }

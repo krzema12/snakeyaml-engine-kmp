7d6ecef0f8f483386f4d9abade0715439cfb19e9
Piotr Krzeminski <git@krzeminski.it>
2025-10-17 23:12:14 +0200

    Add compilation tests for processor

--- copy-dsl-ksp-processor/build.gradle.kts
+++ copy-dsl-ksp-processor/build.gradle.kts
@@ -11,5 +11,16 @@ kotlin {
                 implementation(libs.kotlinpoet.ksp)
             }
         }
+
+        jvmTest {
+            dependencies {
+                implementation(libs.kotest.framework.engine)
+                implementation(libs.kotest.assertions.core)
+                implementation(libs.junit.jupiter.engine)
+                implementation(libs.kotest.runner.junit5)
+
+                implementation(libs.kotlin.compile.testing.ksp)
+            }
+        }
     }
 }

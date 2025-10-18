16639859950ade5e5d411f861d02ba738f6b0021
Piotr Krzeminski <git@krzeminski.it>
2025-10-18 17:40:33 +0200

    Assert on output from compiled test

---
+++ copy-dsl-ksp-processor/src/jvmTest/kotlin/it/krzeminski/copydsl/impl/CommandUtils.kt
@@ -0,0 +1,15 @@
+package it.krzeminski.copydsl.impl
+
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+fun runCommand(vararg args: String): String {
+    val proc = ProcessBuilder(args.toList())
+        .directory(File("."))
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    proc.waitFor(1, TimeUnit.MINUTES)
+    return proc.inputStream.bufferedReader().readText()
+}

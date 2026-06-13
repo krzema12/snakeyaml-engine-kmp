8b1cfd736c731c2bd19b99de19f36eba21fdeb6d
Piotr Krzeminski <git@krzeminski.it>
2026-06-13 22:07:44 +0200

    Port upstream bc68379 (Issue 68) - ParserImpl comments (#3)

--- src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue68/CommentAfterAliasTest.kt
+++ src/commonTest/kotlin/it/krzeminski/snakeyaml/engine/kmp/issues/issue68/CommentAfterAliasTest.kt
@@ -1,12 +1,9 @@
 package it.krzeminski.snakeyaml.engine.kmp.issues.issue68
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
-import io.kotest.matchers.string.shouldContain
 import it.krzeminski.snakeyaml.engine.kmp.api.LoadSettings
 import it.krzeminski.snakeyaml.engine.kmp.api.lowlevel.Compose
-import it.krzeminski.snakeyaml.engine.kmp.exceptions.ParserException
 
 class CommentAfterAliasTest : FunSpec({
     listOf(false, true).forEach { parseComments ->
@@ -17,20 +14,7 @@ class CommentAfterAliasTest : FunSpec({
             val input = """
             |field_with_alias: &alias_name # inline comment 1
             |  555""".trimMargin()
-
-            if (parseComments) {
-                // This test shows a certain undesired behavior that ideally should be fixed one day.
-                // See https://github.com/krzema12/snakeyaml-engine-kmp/issues/478.
-                shouldThrow<ParserException> {
-                    compose.compose(input)
-                }.also {
-                    it.message shouldContain "while parsing a block mapping"
-                    it.message shouldContain "expected <block end>, but found '<scalar>'"
-                }
-            } else {
-                val node = compose.compose(input)
-                node.shouldNotBeNull()
-            }
+            compose.compose(input).shouldNotBeNull()
         }
 
         test("block comment and flat after when parsing comments enabled: $parseComments") {
@@ -39,20 +23,7 @@ class CommentAfterAliasTest : FunSpec({
             |field_with_alias: &alias_name
             |# separate line comment following the alias
             |    555""".trimMargin()
-
-            if (parseComments) {
-                // This test shows a certain undesired behavior that ideally should be fixed one day.
-                // See https://github.com/krzema12/snakeyaml-engine-kmp/issues/478.
-                shouldThrow<ParserException> {
-                    compose.compose(input)
-                }.also {
-                    it.message shouldContain "while parsing a block mapping"
-                    it.message shouldContain "expected <block end>, but found '<scalar>'"
-                }
-            } else {
-                val node = compose.compose(input)
-                node.shouldNotBeNull()
-            }
+            compose.compose(input).shouldNotBeNull()
         }
 
         test("block comment and nested after when parsing comments enabled: $parseComments") {
@@ -61,20 +32,7 @@ class CommentAfterAliasTest : FunSpec({
             |field_with_alias: &alias_name
             |# separate line comment following the alias
             |    nested_field: nested_value""".trimMargin()
-
-            if (parseComments) {
-                // This test shows a certain undesired behavior that ideally should be fixed one day.
-                // See https://github.com/krzema12/snakeyaml-engine-kmp/issues/478.
-                shouldThrow<ParserException> {
-                    compose.compose(input)
-                }.also {
-                    it.message shouldContain "while parsing a block mapping"
-                    it.message shouldContain "expected <block end>, but found '<block mapping start>'"
-                }
-            } else {
-                val node = compose.compose(input)
-                node.shouldNotBeNull()
-            }
+            compose.compose(input).shouldNotBeNull()
         }
     }
 })
